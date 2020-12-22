4271fc95f843e881ff041ca0c44a4d32501b0a67
Sean Brown <seanbrown82@hotmail.com>
2020-12-22 20:07:42 +0000

    working on chaining operations

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ function multiply(num1, num2) {
 
 
 function divide (num1, num2) {
-  
-  return num1 / num2
+
+ return num2 === 0 ? "Can't divide by zero." : num1 / num2
 
 }
 
@@ -78,52 +78,50 @@ display.textContent = displayValue.join("")
 
 
 function operatorFunction() {
-  //If an operator button is clicked, save its value in the operator variable
-  if(event.target.id === "add" || event.target.id === "subtract" ||event.target.id === "multiply" ||event.target.id === "divide") {
+  //If there is no previous result, calculate as normal
+
+  if(memoryValue.length === 0) {
+
+    //Set the operator to the clicked button
+    if(event.target.id === "add" || event.target.id === "subtract" ||event.target.id === "multiply" ||event.target.id === "divide") {
     operator = event.target.id;
+    console.log(operator)
 
-    if(memoryValue.length === 0) {
-      console.log("Nothing in mem")
-    }
+    //Give the clicked button clicked status
 
-    if(memoryValue.length !== 0) {
-      console.log("Something in mem")
-
-      //Try code to say that when something is in memory, run the operator function as normal but save the result in variable and use that in a calculation?
-    }
-
-    
-       //Store the value that is currently displayed in a memory array
-    memoryValue = [...displayValue];
-    
-    //Clear the display array and display screen
-    displayValue = [];
-
-    // Loop through the operators to identify the one that was clicked. Add the clicked class to it.
-
-    functionKeys.forEach(key => {
-      if(key.id === event.target.id) {
-        event.target.classList.add("clicked")
-      }
-
-      //If anything else has clicked status, remove it
-      else if(key.classList.contains("clicked")) {
-        key.classList.remove("clicked")
-
-      }
-
-      else return 
-      
-
+    functionKeys.forEach(item => {
+      item.id == event.target.id ? item.classList.add("clicked") : item.classList.remove("clicked");
     })
 
- //Add this so that decimals can be used again, after an operator has been selected
- if(numbers[10].classList.contains("decimal")){
-  numbers[10].classList.remove("decimal")
-}
+    //Move the value on screen into memory
+
+    memoryValue = [...displayValue];
+
+    //Clear the display array but keep prev value on screen
     
+    displayValue = [];
+
+
   }
-       
+
+}
+//If there is a prev result...
+/*
+else {
+
+  result = operate(operator, Number(memoryValue.join("")), Number(displayValue.join(""))) + ""
+
+  console.log(result)
+  display.textContent = result;
+  displayValue = []
+  console.log(displayValue, "displayed")
+  
+  memoryValue = [...result.split("")]
+  console.log("memory:", memoryValue, "display", displayValue)
+ 
+  
+}*/
+
 
 }
 
@@ -176,6 +174,7 @@ function clearFunction() {
       }
     })
       }
+      
 }
 
 //Bug found - pressing a button then clicking two operators produces an incorrect result. Caused by clear function in runOperator?
@@ -187,3 +186,59 @@ press operator key (calculation is done and result stored somewhere and displaye
 
 
 //Split up the operate section into different functions
+
+
+/* Old operator function - stored for reference
+
+function operatorFunction() {
+  //If an operator button is clicked, save its value in the operator variable
+  if(event.target.id === "add" || event.target.id === "subtract" ||event.target.id === "multiply" ||event.target.id === "divide") {
+    operator = event.target.id;
+
+    if(memoryValue.length === 0) {
+      console.log("Nothing in mem")
+    }
+
+    if(memoryValue.length !== 0) {
+      console.log("Something in mem")
+
+      //Try code to say that when something is in memory, run the operator function as normal but save the result in variable and use that in a calculation?
+    }
+
+    
+       //Store the value that is currently displayed in a memory array
+    memoryValue = [...displayValue];
+    
+    //Clear the display array and display screen
+    displayValue = [];
+
+    // Loop through the operators to identify the one that was clicked. Add the clicked class to it.
+
+    functionKeys.forEach(key => {
+      if(key.id === event.target.id) {
+        event.target.classList.add("clicked")
+      }
+
+      //If anything else has clicked status, remove it
+      else if(key.classList.contains("clicked")) {
+        key.classList.remove("clicked")
+
+      }
+
+      else return 
+      
+
+    })
+
+ //Add this so that decimals can be used again, after an operator has been selected
+ if(numbers[10].classList.contains("decimal")){
+  numbers[10].classList.remove("decimal")
+}
+    
+  }
+       
+
+}
+
+*/
+
